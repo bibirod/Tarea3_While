9ca4092a67e1f6854339490944c566383dfcf094
BibianaR <nbibiro@gmail.com>
2023-06-01 11:04:14 -0500

    Version final Tarea3

--- script.js
+++ script.js
@@ -8,3 +8,55 @@ while (control<=numero) {
  }
    
 }
+//2.
+let numero1=prompt("Dime un numero entre 1 y 50");
+let numero2= prompt("Dime otro numero entre 1 y 50");
+control=1;
+while(control<=50){
+    
+    if(control==numero1 || control==numero2){
+        console.log("LOTERIA");
+    }
+    else{
+        console.log(control);
+    }
+    control=control+1;
+}
+//3.
+let captura=1;
+let arreglo = [];
+while (captura!==0) {
+    captura= prompt("dime un numero");
+    captura= parseInt(captura);
+    if (!isNaN(captura)){
+        arreglo.push(captura);
+
+    }
+   console.log(arreglo); 
+}*/
+//5.
+let dia="vacio";
+while (dia=="domingo") {
+
+    dia=prompt("dime un dia de la semana");
+    if(dia.toLowerCase()=="lunes"){
+        alert("los lunes te atendemos en la tarde");
+    }
+    else if(dia.toLowerCase()=="martes"){
+        alert("hoy es martes de concentración");  
+    }
+    else if(dia.toLowerCase()=="miercoles"){
+        alert("ombligo de semana");  
+    }
+    else if(dia.toLowerCase()=="jueves"){
+        alert("hoy es juernes");  
+    }
+    else if(dia.toLowerCase()=="viernes"){
+        alert("es viernes y el cuerpo lo sabes");  
+    }
+    else if(dia.toLowerCase()=="sabado"){
+        alert("sabado de diversion");  
+    }
+    dia= dia.toLowerCase;
+}
+alert("Ve a descansar");
